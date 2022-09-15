1fa0e237d16397e262c55de332d091220173f67f
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-15 00:29:38 -0300

    Using route group instead of individuals :D

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,4 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.post('/auth', 'AuthController.store')
-Route.delete('/auth', 'AuthController.destroy')
+Route.group(() => {
+  Route.post('/', 'AuthController.store')
+  Route.delete('/', 'AuthController.destroy')
+}).prefix('auth')
