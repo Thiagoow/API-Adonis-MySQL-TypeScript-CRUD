9c0e9e3015809e61ccbb7c1c00487b28daf6b854
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-14 23:21:57 -0300

    View methods removed

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -3,13 +3,9 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class PostsController {
   public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
-
   public async store({}: HttpContextContract) {}
 
   public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
 
   public async update({}: HttpContextContract) {}
 
