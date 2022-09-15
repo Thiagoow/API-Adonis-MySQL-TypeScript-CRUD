2f683ef8675a063bf46047554953bcbf8c205119
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-15 00:25:26 -0300

    Routes splitted, each in its own file

--- start/routes/index.ts
+++ start/routes/index.ts
@@ -1,9 +1,9 @@
 import Route from '@ioc:Adonis/Core/Route'
+import './posts'
+
 /* To check all routes created on this project, run: 
 node ace list:routes */
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return { hello: 'world :D' }
 })
-
-Route.resource('posts', 'PostsController').apiOnly()
