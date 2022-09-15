59d749830713628f7709cc0ff2fe835535ef4242
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-15 00:18:22 -0300

    Auth methods created

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
-  public async store({}: HttpContextContract) {}
+  public async store({ request, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+    const token = await auth.attempt(email, password, {
+      expiresIn: '30 days',
+    })
+    return token
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ auth }: HttpContextContract) {
+    await auth.logout()
+  }
 }
