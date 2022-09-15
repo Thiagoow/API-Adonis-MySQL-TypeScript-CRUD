6cc5380953b8bc9fcf456d7a26f6a561b5adf199
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-15 00:15:22 -0300

    Created authContr with -r but keep only store & destroy

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,7 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async store({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
