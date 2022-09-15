4a7dca2bf594a6cd79a1c25a4d273ceda7b44ac8
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-14 23:36:38 -0300

    CRUD added :D Now it's time to create the routes

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,13 +1,36 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class PostsController {
-  public async index({}: HttpContextContract) {}
+  public async index({}: HttpContextContract) {
+    const posts = await Post.all()
+    return posts
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = await Post.create(data)
+    return post
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
 
-  public async update({}: HttpContextContract) {}
+    return post
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ params, request }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+    //Replace/merge data on post:
+    post.merge(data)
+    //Save on database:
+    await post.save()
+    return post
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    await post.delete()
+  }
 }
