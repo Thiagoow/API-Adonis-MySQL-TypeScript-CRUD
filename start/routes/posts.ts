dba1b4a2b0e23fd43a2b4ed1cfebc784d85d5ed2
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-15 17:48:30 -0300

    Auth middleware configured & added on routes

--- start/routes/posts.ts
+++ start/routes/posts.ts
@@ -1,3 +1,12 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.resource('posts', 'PostsController').apiOnly()
+Route.resource('posts', 'PostsController')
+  .apiOnly()
+  .middleware({
+    //Require auth for these routes:
+    store: ['auth'],
+    update: ['auth'],
+    destroy: ['auth'],
+    /* It's an array because we
+    can have multiple middleware */
+  })
