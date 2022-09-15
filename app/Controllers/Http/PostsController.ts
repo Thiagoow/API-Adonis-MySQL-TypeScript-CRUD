fc940b6c21daf092b46a92f3d62e129fca5be986
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-14 23:20:26 -0300

    Controller deleted & created again with -r flag (that adds auto all resources)

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,3 +1,17 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class PostsController {}
+export default class PostsController {
+  public async index({}: HttpContextContract) {}
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
