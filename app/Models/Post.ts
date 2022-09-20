94e2daf3c44751af8c14e27d0ac00b85707b3150
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-20 01:12:27 -0300

    Date & time formatted using Luxon :D

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -11,9 +11,21 @@ export default class Post extends BaseModel {
   @column()
   public content: string
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({
+    autoCreate: true,
+    // Formatting date & time using Luxon (HH to be 24h):
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy HH:mm:ss')
+    },
+  })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+    serialize: (value: DateTime) => {
+      return value.toFormat('dd/MM/yyyy HH:mm:ss')
+    },
+  })
   public updatedAt: DateTime
 }
