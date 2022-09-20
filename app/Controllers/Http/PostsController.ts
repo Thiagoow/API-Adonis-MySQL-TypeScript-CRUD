742149c161fa9a92549645f0c84a9664b2ac86f6
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-20 16:38:00 -0300

    Loading posts author info on get, store & update

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -4,20 +4,28 @@ import { StoreValidator, UpdateValidator } from 'App/Validators/Post'
 
 export default class PostsController {
   public async index({}: HttpContextContract) {
-    const posts = await Post.all()
-    /* To get on created order:
-     Post.query().orderBy('id') */
+    const posts = await Post.query().orderBy('id', 'asc').preload('author')
+    /* If I don't want to sort the posts or 
+    show the author info, I can just do: 
+    const posts = await Post.all() */
+
     return posts
   }
 
-  public async store({ request }: HttpContextContract) {
+  public async store({ request, auth }: HttpContextContract) {
     const data = await request.validate(StoreValidator)
-    const post = await Post.create(data)
+    const user = await auth.authenticate()
+
+    const post = await Post.create({ authorId: user.id, ...data })
+    //Same name as the relation on Post model:
+    await post.load('author')
+
     return post
   }
 
   public async show({ params }: HttpContextContract) {
     const post = await Post.findOrFail(params.id)
+    await post.load('author')
     return post
   }
 
@@ -28,6 +36,9 @@ export default class PostsController {
     post.merge(data)
     //Save on database:
     await post.save()
+    //Load author relation:
+    await post.load('author')
+
     return post
   }
 
