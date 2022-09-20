e88e17b0de0c0bb925f067f6d7ef25b89719e5a6
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-20 00:37:53 -0300

    Title updated to have rule: unique

--- app/Validators/PostValidator.ts
+++ app/Validators/PostValidator.ts
@@ -1,11 +1,11 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PostValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    title: schema.string({ trim: true }),
+    title: schema.string({ trim: true }, [rules.unique({ table: 'posts', column: 'title' })]),
     content: schema.string({ trim: true }),
   })
 
