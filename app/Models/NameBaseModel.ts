df1b46bf02843ae7f2c0e1caf9b166516df2ebf0
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-20 17:23:14 -0300

    AppBaseModel renamed to NameBaseModel

--- app/Models/NameBaseModel.ts
+++ app/Models/NameBaseModel.ts
@@ -1,6 +1,6 @@
 import { BaseModel } from '@ioc:Adonis/Lucid/Orm'
 import CamelCaseNamingStrategy from 'App/Strategies/CamelCaseNamingStrategy'
 
-export default class AppBaseModel extends BaseModel {
+export default class NameBaseModel extends BaseModel {
   public static namingStrategy = new CamelCaseNamingStrategy()
 }
